1c7e117a3290a50d71ef97ba039530164c26023b
Venechozix <98348449+Venechozix@users.noreply.github.com>
2023-09-21 23:06:37 -0300

    Add files via upload

---
+++ Programacion/SistemaQuickCarry/Calculadora/Camioneros.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaQuickCarry
+{
+    public partial class Camioneros : Form
+    {
+        public Camioneros()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalidaCamion_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Camioneros_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
